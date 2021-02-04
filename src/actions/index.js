01a3f7409d46b1c58522b98f0dd441173305551c
Sumbul Kaynat <sumbulkaynat@gmail.com>
2021-02-05 02:08:56 +0530

    Added the functionality to unfavourite movie and changing tabs

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,3 @@
-import movies from "../reducers";
-
 // {
 //     type: 'ADD_MOVIES',
 //     movies : [m1,m2,m3]
@@ -11,7 +9,11 @@ import movies from "../reducers";
 
 // action types
 export const ADD_MOVIES = 'ADD_MOVIES';
-export const ADD_FAVOURITE = 'ADD_FAVOURITE';
+export const ADD_TO_FAVOURITES = 'ADD_TO_FAVOURITES';
+export const REMOVE_FROM_FAVOURITES = 'REMOVE_FROM_FAVOURITES';
+export const SET_SHOW_FAVOURITES = 'SET_SHOW_FAVOURITES';
+
+
 
 
 export function addMovies(movies) {
@@ -23,7 +25,21 @@ export function addMovies(movies) {
 
 export function addFavourite(movie) {
     return {
-        type: ADD_FAVOURITE,
+        type: ADD_TO_FAVOURITES,
         movie
+    }
+}
+
+export function removeFromFavourites(movie) {
+    return {
+        type: REMOVE_FROM_FAVOURITES,
+        movie
+    }
+}
+
+export function setShowFavourites(val) {
+    return {
+        type: SET_SHOW_FAVOURITES,
+        val
     }
 }
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { addFavourite } from '../actions';
+import { addFavourite , removeFromFavourites } from '../actions';
 
 
 class MovieCard extends React.Component {
@@ -10,7 +10,11 @@ class MovieCard extends React.Component {
         this.props.dispatch(addFavourite(movie));
     }
 
-    handleUnFavouriteClick = () => {}
+    handleUnFavouriteClick = () => {
+        const {movie} =this.props;
+        //console.log(movie);
+        this.props.dispatch(removeFromFavourites(movie));
+    }
 
     render () {
         const {movie,isFavourite} = this.props;
