aab2ab4e043ab57009d91934c2bf9efa58f4c175
Sumbul Kaynat <sumbulkaynat@gmail.com>
2021-02-05 21:53:14 +0530

    Middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
+// function logger(obj, next, action)
+const logger = function ({dispatch, getState }){
+  return function (next) {
+    return function (action) {
+      //middleware code
+      console.log('ACTION_TYPE = ', action.type);
+      next(action);
+    }
+  }
+}
 
-const store = createStore (rootReducer);
+const store = createStore (rootReducer, applyMiddleware(logger));
 console.log('store', store);
 // console.log('before state', store.getState());
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,11 @@
-import { ADD_MOVIES , ADD_TO_FAVOURITES , REMOVE_FROM_FAVOURITES , SET_SHOW_FAVOURITES } from "../actions";
+import { combineReducers } from "redux";
+
+import { 
+    ADD_MOVIES , 
+    ADD_TO_FAVOURITES , 
+    REMOVE_FROM_FAVOURITES , 
+    SET_SHOW_FAVOURITES 
+} from "../actions";
 
 const initialMovieState = {
     list : [] ,
@@ -62,9 +69,14 @@ const initialRootState = {
     search: initialSearchState
 };
 
-export default function rootReducer (state = initialRootState, action) {
-    return {
-        movies : movies(state.movies , action),
-        search: search(state.search , action)
-    }
-};
+// export default function rootReducer (state = initialRootState, action) {
+//     return {
+//         movies : movies(state.movies , action),
+//         search: search(state.search , action)
+//     }
+// };
+
+export default combineReducers ({
+    movies,
+    search
+})
