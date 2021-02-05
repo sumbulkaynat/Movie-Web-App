9f91be79b49aab14a2d1857501e79ce38fea0691
Sumbul Kaynat <sumbulkaynat@gmail.com>
2021-02-05 22:00:19 +0530

    Modifying Middleware

--- src/index.js
+++ src/index.js
@@ -7,14 +7,20 @@ import App from './components/App';
 import rootReducer from './reducers';
 
 // function logger(obj, next, action)
-const logger = function ({dispatch, getState }){
-  return function (next) {
-    return function (action) {
-      //middleware code
-      console.log('ACTION_TYPE = ', action.type);
-      next(action);
-    }
-  }
+// const logger = function ({dispatch, getState }){
+//   return function (next) {
+//     return function (action) {
+//       //middleware code
+//       console.log('ACTION_TYPE = ', action.type);
+//       next(action);
+//     }
+//   }
+// }
+
+//another form of writing same above middleware
+const logger = ({dispatch, getState }) => (next) => (action) => {
+  console.log('ACTION_TYPE = ', action.type);
+  next(action);
 }
 
 const store = createStore (rootReducer, applyMiddleware(logger));
